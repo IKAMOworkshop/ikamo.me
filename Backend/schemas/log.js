c20189362c0ba2743df9cba9c9416a9852ba0092
Jeremy Chang <changjeremy0226@gmail.com>
2023-09-29 23:28:46 -0700

    Update title and name for data structure files

--- Backend/schemas/log.js
+++ Backend/schemas/log.js
@@ -1,8 +1,8 @@
 import {defineField, defineType} from 'sanity'
 
 export default defineType({
-  name: 'post',
-  title: 'Post',
+  name: 'logs',
+  title: 'Logs',
   type: 'document',
   fields: [
 
--- Backend/schemas/tags.js
+++ Backend/schemas/tags.js
@@ -1,8 +1,8 @@
 import {defineField, defineType} from 'sanity'
 
 export default defineType({
-  name: 'category',
-  title: 'Category',
+  name: 'tags',
+  title: 'Tags',
   type: 'document',
   fields: [
     defineField({
