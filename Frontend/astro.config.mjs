1f005570dcfb09d69d5ef9753a64f8703f93bcaa
Jeremy Chang <changjeremy0226@gmail.com>
2023-10-03 02:53:55 -0700

    Log layout completed

--- Frontend/astro.config.mjs
+++ Frontend/astro.config.mjs
@@ -1,4 +1,13 @@
 import { defineConfig } from 'astro/config';
 
+import sanity from "astro-sanity";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [sanity({
+    projectId: '44xgurt2',
+    dataset: 'production',
+    apiVersion: 'v2021-03-25',
+    useCdn: true,
+  })]
+});
