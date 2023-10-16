43b9b6c667bce357dc44402e6e8c86cfdc079061
Jeremy Chang <changjeremy0226@gmail.com>
2023-10-16 02:26:32 -0700

    Log sort by date (new to old) implemented.

--- Frontend/src/sanity/api.js
+++ Frontend/src/sanity/api.js
@@ -6,3 +6,19 @@ export async function getAllPosts() {
     const posts = await client.fetch(query);
     return posts;
 };
+
+// Date Sorting
+export function formatLogPost(posts, {
+	sortByDate = true,
+} = {}) {
+    if (sortByDate) {
+        posts.sort((a,b) => {
+            const dateA = new Date(a.eventDate);
+            const dateB = new Date(b.eventDate);
+    
+            return dateB - dateA;
+        });
+    };
+    console.log(posts);
+	return posts;
+};
